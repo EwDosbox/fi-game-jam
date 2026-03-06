7db901aa6c63c52c31772df9c27f4a44e8631cab
EwDosbox <cada.vviktor@gmail.com>
2026-03-06 20:13:30 +0100

    basic level logic

---
+++ src/Scripts/Game.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Game : Node2D
+{
+	#region Vars
+
+	private double timeElapsed = 0;
+
+	#endregion
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+
+	private void GameTick()
+	{
+
+	}
+}
