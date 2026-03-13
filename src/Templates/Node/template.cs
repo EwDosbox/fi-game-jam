0b2693ea5e2c1ffdf768c19501e0dfd0e04f561b
EwDosbox <cada.vviktor@gmail.com>
2026-03-13 19:50:40 +0100

    Icons and Templates

---
+++ src/Templates/Node/template.cs
@@ -0,0 +1,28 @@
+// meta-name: Basic Template
+// meta-description: Predefined movement for classical platformers
+// meta-default: true
+// meta-space-indent: 4
+using _BINDINGS_NAMESPACE_;
+using System;
+
+public partial class _CLASS_ : _BASE_
+{
+
+    #region Private Variables
+    #endregion
+    #region Public Variables
+    #endregion
+    #region References
+    #endregion
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+    }
+}
